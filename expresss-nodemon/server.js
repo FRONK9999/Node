c4eb58ab0e159563cdcb23441dabb474d6b013fd
Jhuan Lucas <59448649+FRONK9999@users.noreply.github.com>
2023-11-13 01:03:54 -0300

    nodemon

---
+++ expresss-nodemon/server.js
@@ -0,0 +1,31 @@
+const express = require('express');	
+const app = express();
+
+
+//CRUD -> CREATE, READ, UPDATE, DELETE
+//        POST    GET   PUT     DELETE
+// http://meusite.com/ <- get -> deliver the page
+// http://meusite.com/sobre <- get -> deliver the page /about
+// http://meusite.com/contato <- get -> deliver the page /contact
+
+app.get('/', (req, res) => {
+    res.send(`
+        <form action="/" method="post">
+            client name: <input type="text" name="name">
+            <button>Enviar</button>     
+        </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário');
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Obrigado por entrar em contato com a gente.');
+});
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
