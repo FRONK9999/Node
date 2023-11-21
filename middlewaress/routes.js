7cb54c7282bee0eec8e3d5c332d1dc4ee5626bc2
Jhuan Lucas <59448649+FRONK9999@users.noreply.github.com>
2023-11-21 00:44:33 -0300

    middlewaress

---
+++ middlewaress/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = express.Router();
+const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contactController');
+
+
+// Rotas da home
+route.get('/', homeController.paginaInicial)
+route.post('/', homeController.trataPost);
+
+// contact routes 
+route.get('/contact', contatoController.paginaInicial)
+module.exports = route
